11c1d7148f63888b482a69e85338de054264bb2c
Pedro Lima <pedrogglima@gmail.com>
2021-04-11 16:54:39 -0300

    update header with avatar and github link

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,31 @@
 import * as React from "react"
 import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
 const Header = ({ siteTitle }) => (
   <header>
-    <nav className="flex items-center justify-between flex-wrap p-6">
+    <nav className="flex items-center justify-between flex-wrap p-4">
       <div className="flex items-center flex-no-shrink text-white mr-6">
+        <StaticImage
+          src="../images/me-about.jpeg"
+          width={50}
+          quality={95}
+          formats={["AUTO", "WEBP", "AVIF"]}
+          alt="Me - photo"
+          style={{ marginBottom: `0 rem`, borderRadius: "100%" }}
+        />
         <span className="mx-4 font-semibold text-xl tracking-tight">
           {siteTitle}
         </span>
+      </div>
+      <div className="flex items-center flex-grow"></div>
+      <div className=" text-white">
+        <Link to="https://github.com/pedrogglima">
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </Link>
       </div>
     </nav>
   </header>
@@ -18,7 +36,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: `@pedrogglima`,
+  siteTitle: `pedrogglima`,
 }
 
 export default Header
