e89b05a998db61a5ee45120780e2839ddfc58941
Pedro Lima <pedrogglima@gmail.com>
2021-04-11 11:07:17 -0300

    update layout with footer component

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,28 +9,15 @@ import * as React from "react"
 import PropTypes from "prop-types"
 
 import Header from "./header"
+import Footer from "./footer"
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle="@pedrogglima" />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          pedrogglima © 2021-{new Date().getFullYear()} - Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">gatsby</a>
-        </footer>
+        <Footer author="pedrogglima"></Footer>
       </div>
     </>
   )
